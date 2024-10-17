e78f245281d7eb693656da3b282080e35ecfc72f
Tanmaydeep Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2024-10-18 01:41:17 +0530

    Update events.ts

--- src/constants/events.ts
+++ src/constants/events.ts
@@ -6,4 +6,6 @@ export const EVENTS
     { id: 4, title: 'Upcoming Event 2', date: '2024-11-05', description: 'Description for upcoming event 2', link: '#', image: 'https://example.com/image4.jpg' },
     { id: 5, title: 'Recent Event 3', date: '2024-09-25', description: 'Description for recent event 3', link: '#', image: 'https://example.com/image5.jpg' },
     { id: 6, title: 'Upcoming Event 3', date: '2024-12-15', description: 'Description for upcoming event 3', link: '#', image: 'https://example.com/image6.jpg' },
+    { id: 7, title: 'Upcoming Event 4', date: '2024-12-15', description: 'Description for upcoming event 4', link: '#', image: 'https://example.com/image6.jpg' },
+
   ];
