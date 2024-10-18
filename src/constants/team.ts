aca71f274c7d5ce89c62499f466012634e692a9a
Brahamdeep <brahamdeep9@gmail.com>
2024-10-18 21:34:49 +0530

    Updated emails in the config JSON

--- src/constants/team.ts
+++ src/constants/team.ts
@@ -2,49 +2,49 @@ export const TEAM = [
     {
         name:'Eshaa Bhasin',
         position:'Lead',
-        email:'',
+        email:'eshaabhasin4@gmail.com',
         github:'https://github.com/Eshaabhasin',
         linkedIn:'https://www.linkedin.com/in/eshaabhasin/'
     },
     {
         name:'Avi Kapoor',
         position:'Co-Lead',
-        email:'',
+        email:'avikapoor118@gmail.com',
         github:'https://github.com/avikapoor24',
         linkedIn:'https://www.linkedin.com/in/avikapoor16/'
     },
     {
         name:'Brahamdeep Singh',
         position:'Operations Lead',
-        email:'',
+        email:'brahamdeep9@gmail.com',
         github:'https://github.com/Brahamdeep',
         linkedIn:'https://www.linkedin.com/in/brahamdeep-singh-sabharwal-14a914256/'
     },
     {
         name:'Harshpreet Singh',
         position:'Managment Lead',
-        email:'',
+        email:'harshps1001@gmail.com',
         github:'',
         linkedIn:'https://www.linkedin.com/in/harshps1001/'
     },
     {
         name:'Mukal Dadhwal',
         position:'Tech Lead',
-        email:'',
+        email:'mukaldadhwal@gmail.com',
         github:'https://github.com/MukalDadhwal',
         linkedIn:'https://www.linkedin.com/in/mukal-dadhwal/'
     },
     {
         name:'Gursheel Kaur',
         position:'Design Lead',
-        email:'',
+        email:'gursheelkaur7@gmail.com',
         github:'',
         linkedIn:'https://www.linkedin.com/in/gursheel-kaur-/'
     },
     {
         name:'Ishwardeep Singh',
         position:'Growth and Innovation Lead',
-        email:'',
+        email:'ishwardeep.work@gmail.com',
         github:'https://github.com/ishwardeepp',
         linkedIn:'https://www.linkedin.com/in/ishwardeep-singh-405a9324a/'
     }
