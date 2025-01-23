3987a36a84e237e2199a52626c19f0106e0c670a
Mandeep Singh <singh.mandeeep2007@gmail.com>
2025-01-24 00:39:52 +0530

    Create banner.tsx

---
+++ src/app/banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MeetTheTeam from '../teamCard';
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <MeetTheTeam />
+    </div>
+  );
+};
+
+export default AboutPage;
